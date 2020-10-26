c9c67551821225b867b0c0e90acc69d71fed8526
jaehak9078 <jaehak9078@gmail.com>
2020-10-26 13:57:29 +0900

    v2

--- src/main/java/com/cos/jwt/domain/post/BoardRepository.java
+++ src/main/java/com/cos/jwt/domain/post/BoardRepository.java
@@ -1,8 +1,15 @@
 package com.cos.jwt.domain.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board,Integer>{
 
+	@Query(value = "SELECT * FROM board WHERE bno = :bno",nativeQuery = true)
+	Board FindByBno(int bno);
 	
+	@Modifying
+	@Query(value = "DELETE FROM board WHERE bno =:bno",nativeQuery = true)
+	int DeleteByBno(int bno);
 }
